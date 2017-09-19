5dcf6ca5d08cf5c2fd1d3e8d1283edc91e7e719a
timmcallister <tim.mcallister84@gmail.com>
2017-09-18 19:32:34 -0500

    Added time class, added click listener to run screen

--- app/src/main/java/timsthings/pacer/RunScreen.java
+++ app/src/main/java/timsthings/pacer/RunScreen.java
@@ -2,18 +2,36 @@ package timsthings.pacer;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class RunScreen extends AppCompatActivity {
+public class RunScreen extends AppCompatActivity implements View.OnClickListener {
 
-
+    Button startRecord;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_run_screen);
 
+        startRecord = (Button) findViewById(R.id.startRecord);
+        startRecord.setOnClickListener(this);
+
         //// TODO: 9/11/17 Display TGT lap and total times
 
         //// TODO: 9/11/17 Implement core logic.
+    }
+
+    @Override
+    public void onClick(View view) {
+
+        switch (view.getId()){
+
+
+
+            default:
+                break;
+        }
+
     }
 }
---
+++ app/src/main/java/timsthings/pacer/Time.java
@@ -0,0 +1,8 @@
+package timsthings.pacer;
+
+/**
+ * Created by tim on 9/18/17.
+ */
+
+public class Time {
+}
